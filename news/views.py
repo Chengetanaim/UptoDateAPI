6398e8625056e8f98d94df9bc90edd610ec43034
Chengetanaim <87484844+Chengetanaim@users.noreply.github.com>
2022-09-01 13:04:39 +0200

    Update views.py

--- news/views.py
+++ news/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from .serializers import NewsDataSerializer, JobSerializer
 from .models import NewsData, Job
 from rest_framework import generics
-# from .jobs import job_links, company_names, descriptions, job_types, dates_added
-# from .newsdata import titles, website_links
-# import itertools
+from .jobs import job_links, company_names, descriptions, job_types, dates_added
+from .newsdata import titles, website_links
+import itertools
 
-# def addnews():
-#     for (title1, website_link1) in zip(titles, website_links):
-#         new = NewsData(title=title1, website_link=website_link1)
-#         new.save()
+def addnews():
+    for (title1, website_link1) in zip(titles, website_links):
+        new = NewsData(title=title1, website_link=website_link1)
+        new.save()
 
-# addnews()
+addnews()
 
-# def addjob():
-#     for (job_link, company_name, description, job_type, date_posted) in zip(job_links, company_names, descriptions, job_types, dates_added):
-#         new_job = Job(company_name=company_name, description=description, website_link=job_link, job_type=job_type, date_posted=date_posted)
-#         new_job.save()
+def addjob():
+    for (job_link, company_name, description, job_type, date_posted) in zip(job_links, company_names, descriptions, job_types, dates_added):
+        new_job = Job(company_name=company_name, description=description, website_link=job_link, job_type=job_type, date_posted=date_posted)
+        new_job.save()
 
-# addjob()
+addjob()
 
 
 
