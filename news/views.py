7be6f984747e06e5816975064a19ec5ccf051fdf
Chengetanaim <87484844+Chengetanaim@users.noreply.github.com>
2022-09-01 17:47:16 +0200

    Update views.py

--- news/views.py
+++ news/views.py
@@ -13,22 +13,24 @@ def addnews():
 
 
 
+
 def addjob():
     for (job_link, company_name, description, job_type, date_posted) in zip(job_links, company_names, descriptions, job_types, dates_added):
         new_job = Job(company_name=company_name, description=description, website_link=job_link, job_type=job_type, date_posted=date_posted)
         new_job.save()
 
+        
+addnews()
+addjob()
 
 
 
 class NewsDataList(generics.ListAPIView):
-    addnews()
     queryset = NewsData.objects.order_by('-id')
     serializer_class = NewsDataSerializer
 
 
 class JobList(generics.ListAPIView):
-    addjob()
     queryset = Job.objects.all()
     serializer_class = JobSerializer
 
