94945324e09ebbd2d5587116b010382de9c71b0e
Chengetanaim <cmukanhairi@gmail.com>
2022-09-01 17:59:55 +0200

    fourth commit

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewsDataList, JobList
+from .views import NewsDataList, JobList, index
 
 
 urlpatterns = [
     path('news-data/', NewsDataList.as_view()),
     path('jobs/', JobList.as_view()),
+    path('index/', view=index, name='index')
 ]
--- news/views.py
+++ news/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .serializers import NewsDataSerializer, JobSerializer
 from .models import NewsData, Job
 from rest_framework import generics
-# from .jobs import job_links, company_names, descriptions, job_types, dates_added
-# from .newsdata import titles, website_links
-# import itertools
+from .jobs import job_links, company_names, descriptions, job_types, dates_added
+from .newsdata import titles, website_links
+import itertools
+import time
 
-# def addnews():
-#     for (title1, website_link1) in zip(titles, website_links):
-#         new = NewsData(title=title1, website_link=website_link1)
-#         new.save()
+def addnews():
+    for (title1, website_link1) in zip(titles, website_links):
+        new = NewsData(title=title1, website_link=website_link1)
+        new.save()
 
-# addnews()
-
-# def addjob():
-#     for (job_link, company_name, description, job_type, date_posted) in zip(job_links, company_names, descriptions, job_types, dates_added):
-#         new_job = Job(company_name=company_name, description=description, website_link=job_link, job_type=job_type, date_posted=date_posted)
-#         new_job.save()
-
-# addjob()
-
+def addjob():
+    for (job_link, company_name, description, job_type, date_posted) in zip(job_links, company_names, descriptions, job_types, dates_added):
+        new_job = Job(company_name=company_name, description=description, website_link=job_link, job_type=job_type, date_posted=date_posted)
+        new_job.save()
 
 
 class NewsDataList(generics.ListAPIView):
@@ -28,7 +25,14 @@ class NewsDataList(generics.ListAPIView):
 
 
 class JobList(generics.ListAPIView):
-    queryset = Job.objects.all()
+    queryset = Job.objects.order_by('-id')
     serializer_class = JobSerializer
+
+
+@login_required
+def index(request):
+    addnews()
+    addjob()
+    return render(request, "news/index.html")
 
 
